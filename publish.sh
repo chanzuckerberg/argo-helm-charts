b061f0ab3b9e4a2da45abb02be74a10928daa46d
Alex Lokshin <alokshin@chanzuckerberg.com>
2024-01-03 17:10:29 -0500

    Bump

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 helm package basic --destination .
-helm repo index . --url https://github.com/chanzuckerberg/argo-helm-charts
+helm repo index . --url https://github.com/chanzuckerberg/argo-helm-charts/blob/main
