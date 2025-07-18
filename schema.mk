eee42359b86024c3a562d5047389cd02a047f94e
Alex Lokshin <alokshin@chanzuckerberg.com>
2025-07-18 12:21:51 -0400

    bump

--- schema.mk
+++ schema.mk
@@ -1,4 +1,4 @@
 
 .PHONY: update-schema
 update-schema:
-	helm schema -input values.yaml -indent 2 -schemaRoot.id argo.helm.charts/$${PWD##*/} -schemaRoot.title $${PWD##*/} -schemaRoot.additionalProperties false
+	helm schema --values values.yaml --indent 2 --schema-root.id argo.helm.charts/${PWD##*/} --schema-root.title ${PWD##*/} --no-additional-properties
