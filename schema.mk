eed34834e6ecac6f687a0d8775ae1b4c88ca9ff8
Allison Doami <adoami@chanzuckerberg.com>
2025-09-05 12:12:34 -0700

    fix: Add back --no-additional-properties

--- schema.mk
+++ schema.mk
@@ -4,4 +4,4 @@
 CHART_NAME := $(shell basename $(PWD))
 
 update-schema:
-	helm schema --values values.yaml --indent 2 --schema-root.id argo.helm.charts/$(CHART_NAME) --schema-root.title $(CHART_NAME)
+	helm schema --values values.yaml --indent 2 --no-additional-properties --schema-root.id argo.helm.charts/$(CHART_NAME) --schema-root.title $(CHART_NAME)
